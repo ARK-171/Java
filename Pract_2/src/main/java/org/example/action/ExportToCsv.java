fcf68a848b3858f0e3bf77974bfcdd088681c0cb
ARK-171 <75318627+ARK-171@users.noreply.github.com>
2022-12-31 18:12:02 +0300

    3

--- Pract_2/src/main/java/org/example/action/ExportToCsv.java
+++ Pract_2/src/main/java/org/example/action/ExportToCsv.java
@@ -10,12 +10,18 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 
 public class ExportToCsv extends AbstractAction {
     private String file = "export.csv";
-    private ObjectMapper mapper= new ObjectMapper();
+//    private ObjectMapper mapper= new ObjectMapper();
+//    private File f = new File("file");
+    Path path = Paths.get(file);
     private CurrencyCsv l;
     private Component parent;
     private List<CurrencyExchange> list = DataBase.getInstance().getCur();
@@ -29,14 +35,17 @@ public class ExportToCsv extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent event) {
-        String s = "id, value , nominal , name , code , date \n";
-        for(int i =0; i<34; i++){
-            s = s + list.get(i).getId().toString() + " , " + list.get(i).getValue().toString() + " , " + list.get(i).getNominal().toString() + " , " + list.get(i).getCurrencyName() + " , " + list.get(i).getCurrencyCode() + " , " + list.get(i).getDate().toString() + " \n ";
-        }
-        l = new CurrencyCsv(s);
+
+//        l = new CurrencyCsv(s);
         try {
+            String s = "id, value , nominal , name , code , date \n";
+            BufferedWriter bw = Files.newBufferedWriter(path, StandardCharsets.UTF_8);
+            for(int i =0; i<34; i++){
+                s = s + list.get(i).getId().toString() + " , " + list.get(i).getValue().toString() + " , " + list.get(i).getNominal().toString() + " , " + list.get(i).getCurrencyName() + " , " + list.get(i).getCurrencyCode() + " , " + list.get(i).getDate().toString() + "\n";
+                bw.write(s);
+            }
             System.out.println(s);
-            mapper.writeValue(new File(file),l);
+//            mapper.writeValue(new File(file),l);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
